f921dd5a7a1fd2eda25b7f22145ef4a6eb90421c
Martin Bernstorff <martinbernstorff@gmail.com>
2023-03-05 12:16:30 +0100

    feat: handle git setup in setup_for_dev

--- {{cookiecutter.project_name}}/setup_for_dev.sh
+++ {{cookiecutter.project_name}}/setup_for_dev.sh
@@ -1,4 +1,12 @@
-virtualenv .venv
+python -m venv .venv
 source .venv/bin/activate
-pip install -e .[dev,tests]
+
+# If no commits exist, initialise the repo
+if ! git rev-parse HEAD > /dev/null 2>&1; then
+  git init
+  git add .
+  git commit -m "init: first commit"
+fi
+
 pre-commit install --install-hooks -t pre-push
+pip install -e .[dev,tests]
