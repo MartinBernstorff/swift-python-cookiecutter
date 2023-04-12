27eebb98d6d3dc249c160e9b1965917cd6b6495b
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-12 21:41:21 +0200

    misc. from review

--- test_template_creation.sh
+++ test_template_creation.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 pip install cruft
+
+# create project using template
 cruft create . -y 
+
+# run setup (install, lint, etc.)
 cd swift-python
 chmod +x .github/first_setup.sh
 sh .github/first_setup.sh
