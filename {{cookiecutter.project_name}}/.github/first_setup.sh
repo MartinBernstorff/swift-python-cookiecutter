f8c934935985ee7498dc7661995743d972c6ee81
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-12 19:09:24 +0200

    misc.

--- {{cookiecutter.project_name}}/.github/first_setup.sh
+++ {{cookiecutter.project_name}}/.github/first_setup.sh
@@ -1,4 +1,9 @@
+# Bash file for automatically doing the full setup on first run
+# Super handy when checking if the template works locally,
+# Also used by create_new_project.yml in git workflows
 pip install invoke
 inv setup
 source .venv39/bin/activate
 inv install
+inv lint
+inv docs
