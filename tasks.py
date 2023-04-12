ef678b8440d79b0404057fbdcc433e3fb52a6f74
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-12 22:11:16 +0200

    ci: have to setup new git repo before installing pre-commit

--- tasks.py
+++ tasks.py
@@ -27,7 +27,6 @@ def test(c: Context):
 
     c.run("pip install cruft")
     c.run("cruft create . -y")
-    c.run("pip install pre-commit")
 
-    for invoke_command in ["lint", "setup", "install", "test", "docs"]:
+    for invoke_command in ["setup", "lint", "install", "test", "docs"]:
         c.run(f"cd {new_instance_dir} && inv {invoke_command}")
