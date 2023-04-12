9a90200fed2b3ebc8ed445f92cf64236e57fbda7
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-12 19:54:07 +0200

    ci: exit setup if error

--- {{cookiecutter.project_name}}/.github/first_setup.sh
+++ {{cookiecutter.project_name}}/.github/first_setup.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 # Bash file for automatically doing the full setup on first run
 # Super handy when checking if the template works locally,
 # Also used by create_new_project.yml in git workflows
+set -e
 pip install invoke
 inv setup
 source .venv39/bin/activate
