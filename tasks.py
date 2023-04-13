6f525c0f1ffeff7b26797345e5baf35a39135aea
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-13 07:42:03 +0200

    ci: refactor

--- tasks.py
+++ tasks.py
@@ -18,15 +18,26 @@ If you do not wish to use invoke you can simply delete this file.
 
 from invoke import Context, task
 
+new_instance_dir = "swift-python"
+
 
 @task
-def test(c: Context):
-    """Run tests of instantiating the project."""
-    new_instance_dir = "swift-python"
+def test_instantiation(c: Context):
+    """Test that the project can be instantiated."""
+    cruft_create(c)
+    setup(c)
+
+
+@task
+def cruft_create(c):
     c.run(f"rm -rf {new_instance_dir}")
 
     c.run("pip install cruft")
     c.run("cruft create . -y")
 
+
+@task
+def setup(c: Context):
+    """Setup while instantiating the project."""
     for invoke_command in ["setup", "install", "lint", "test", "docs"]:
         c.run(f"cd {new_instance_dir} && inv {invoke_command}")
