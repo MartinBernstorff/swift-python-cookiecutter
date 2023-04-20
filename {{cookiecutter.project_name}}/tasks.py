f12415b99b9bedf4c54413d0c7d16ce3be27fe1d
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-20 12:24:38 +0200

    misc.

--- {{cookiecutter.project_name}}/tasks.py
+++ {{cookiecutter.project_name}}/tasks.py
@@ -250,7 +250,9 @@ def static_type_checks(c: Context):
 
 
 @task
-def install(c: Context, pip_args: str = "", msg: bool = True, venv_path: Optional[str] = None):
+def install(
+    c: Context, pip_args: str = "", msg: bool = True, venv_path: Optional[str] = None
+):
     """Install the project in editable mode using pip install"""
     if msg:
         echo_header(f"{msg_type.DOING} Installing project")
@@ -258,7 +260,11 @@ def install(c: Context, pip_args: str = "", msg: bool = True, venv_path: Optiona
     extras = ".[dev,tests,docs]" if NOT_WINDOWS else ".[dev,tests,docs]"
     install_cmd = f"pip install -e {extras} {pip_args}"
 
-    venv_cmd = f"source {venv_path}/bin/activate" if NOT_WINDOWS else f"source {venv_path}/Scripts/activate"
+    venv_cmd = (
+        f"source {venv_path}/bin/activate"
+        if NOT_WINDOWS
+        else f"source {venv_path}/Scripts/activate"
+    )
 
     if venv_path is not None:
         with c.prefix(venv_cmd):
@@ -274,8 +280,10 @@ def get_python_path(preferred_version: str):
     if preferred_version_path is not None:
         return preferred_version_path
     else:
-        input(f"{msg_type.WARN}: python{preferred_version} not found, do you want to continue using default python? (y/n)"})
-        
+        input(
+            f"{msg_type.WARN}: python{preferred_version} not found, do you want to continue using default python? (y/n)"
+        )
+
         if "y" in input.lower():
             return shutil.which("python")
         else:
@@ -334,7 +342,9 @@ def test(c: Context):
         echo_header("Failed tests")
 
         # Get lines with "FAILED" in them from the .pytest_results file
-        failed_tests = [line for line in test_result.stdout if line.startswith("FAILED")]
+        failed_tests = [
+            line for line in test_result.stdout if line.startswith("FAILED")
+        ]
 
         for line in failed_tests:
             # Remove from start of line until /test_
