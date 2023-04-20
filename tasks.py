c109aaa47153a264648f800db7c016f466abde7e
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-20 12:31:17 +0200

    feat: set no line-length for black

--- tasks.py
+++ tasks.py
@@ -45,7 +45,7 @@ def test_instantiation(c: Context):
 
 @task
 def lint(c):
-    c.run("black . --line-length 1000")
+    c.run("black .")
     c.run("ruff check . --isolated --fix --line-length=1000")
     # --isolated to ignore pyproject.toml with cookiecutter placeholders, which are not valid TOML
 
